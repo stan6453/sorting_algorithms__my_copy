1c401584e9d561d2f70af97073b4c50967c85f16
stanley <stanley6453@gmail.com>
2023-01-20 12:19:53 +0100

    fiex betty errors

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,13 +1,14 @@
 #include "sort.h"
 
 /**
- * bubble_sort - sorts an array of integers in ascending order using the Bubble sort algorithm
+ * bubble_sort - sorts an array of integers in
+ * ascending order using the Bubble sort algorithm
  * @array: array to be sorted
  * @size: size of the array
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i = 0, j = 0; 
+	size_t i = 0, j = 0;
 	int swapped = 0, temp;
 
 	for (i = 0; i < size; i++)
@@ -23,7 +24,7 @@ void bubble_sort(int *array, size_t size)
 				print_array(array, size);
 			}
 		}
-		if (swapped)
+		if (!swapped)
 			break;
 	}
 }
