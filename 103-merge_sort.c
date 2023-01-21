ec24ce3928c5fd28a0c89d4e3937d83f5bce5c8b
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2023-01-21 16:45:15 +0100

    merge sort implementation

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -1,59 +1,85 @@
 #include "sort.h"
+void merge(int *leftArr, int leftSize, int *rightArr,
+		   int rightSize, int *array);
+/**
+ * merge_sort - merge sort algorithm
+ * @array: array to sort
+ * @size: size of array
+ *
+ * Return: void
+ */
+void merge_sort(int *array, size_t size)
+{
 
-void merge(int arr[], int left[], int leftSize, int right[], int rightSize) {
-    int i = 0, j = 0, k = 0;
-    printf("[left]: ");
-    for (int p = 0; p < leftSize; p++) {
-        printf("%d, ", left[p]);
-    }
-    printf("\n");
-    printf("[right]: ");
-    for (int p = 0; p < rightSize; p++) {
-        printf("%d, ", right[p]);
-    }
-    printf("\n");
+	size_t mid = size / 2;
+	size_t i = 0;
+	int leftSize = mid;
+	int rightSize = size - mid;
+	int leftArr[4096];
+	int rightArr[4096];
 
-    while (i < leftSize && j < rightSize) {
-        if (left[i] < right[j]) {
-            arr[k] = left[i];
-            i++;
-        } else {
-            arr[k] = right[j];
-            j++;
-        }
-        k++;
-    }
-    while (i < leftSize) {
-        arr[k] = left[i];
-        i++;
-        k++;
-    }
-    while (j < rightSize) {
-        arr[k] = right[j];
-        j++;
-        k++;
-    }
-    printf("[Done]: ");
-    for (int p = 0; p < k; p++) {
-        printf("%d, ", arr[p]);
-    }
-    printf("\nMerging...\n");
+	if (size < 2)
+		return;
+
+	for (i = 0; i < mid; i++)
+	{
+		leftArr[i] = array[i];
+	}
+	for (i = mid; i < size; i++)
+	{
+		rightArr[i - mid] = array[i];
+	}
+
+	merge_sort(leftArr, leftSize);
+	merge_sort(rightArr, rightSize);
+	printf("Merging...\n");
+	printf("[left]: ");
+	print_array(leftArr, leftSize);
+	printf("[right]: ");
+	print_array(rightArr, rightSize);
+	merge(leftArr, leftSize, rightArr, rightSize, array);
+	printf("[Done]: ");
+	print_array(array, size);
 }
 
-void merge_sort(int *arr, size_t size) {
-    if (size < 2) {
-        return;
-    }
-    int mid = size / 2;
-    int left[mid];
-    int right[size - mid];
-    for (int i = 0; i < mid; i++) {
-        left[i] = arr[i];
-    }
-    for (int i = mid; i < size; i++) {
-        right[i - mid] = arr[i];
-    }
-    merge_sort(left, mid);
-    merge_sort(right, size - mid);
-    merge(arr, left, mid, right, size - mid);
+/**
+ * merge - merge function in merge_sort
+ * @leftArr: left array
+ * @leftSize: size of left array
+ * @rightArr: right array
+ * @rightSize: size of right array
+ * @array: array to merge in place
+ */
+void merge(int *leftArr, int leftSize, int *rightArr,
+		   int rightSize, int *array)
+{
+	int i = 0, j = 0, k = 0;
+
+	while (i < leftSize && j < rightSize)
+	{
+		if (leftArr[i] <= rightArr[j])
+		{
+			array[k] = leftArr[i];
+			i++;
+			k++;
+		}
+		else
+		{
+			array[k] = rightArr[j];
+			k++;
+			j++;
+		}
+	}
+	while (i < leftSize)
+	{
+		array[k] = leftArr[i];
+		k++;
+		i++;
+	}
+	while (j < rightSize)
+	{
+		array[k] = rightArr[j];
+		k++;
+		j++;
+	}
 }
